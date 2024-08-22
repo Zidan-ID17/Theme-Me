f27184911c5238d6ed75e221d0c50f38d56a618e
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-08-22 20:01:40 +0800

    Create logo.py

---
+++ support/logo.py
@@ -0,0 +1,33 @@
+# Credit By Zidan IDz
+from lib import merah, putih, abu, pxm, netral, kuning
+
+
+theme-me = (f"""
+{merah}████████╗██╗  ██╗███████╗{putih}███╗   ███╗███████╗
+{merah}╚══██╔══╝██║  ██║██╔════╝{putih}████╗ ████║██╔════╝
+{merah}   ██║   ███████║█████╗  {putih}██╔████╔██║█████╗  
+{merah}   ██║   ██╔══██║██╔══╝  {putih}██║╚██╔╝██║██╔══╝  
+{merah}   ██║   ██║  ██║███████╗{putih}██║ ╚═╝ ██║███████╗
+{merah}   ╚═╝   ╚═╝  ╚═╝╚══════╝{putih}╚═╝     ╚═╝╚══════╝
+{abu}               .: B E T A :.
+
+{kuning} • {putih}Coded by: {pxm} ZidanIDz {netral}
+{kuning} • {putih}YouTube : ZeyShyy.
+{kuning} • {putih}Github  : https://github.com/Zidan-ID17 
+{abu}———————————————————————————————————————————""")
+
+lst_clr = """{abu}
+     Some List of HTML & CSS Color Codes
+┏━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━━━━━┳━━━━━━━━━┓
+┃ White   : #FFFFFF ┃ Light Coral    : #F08080 ┃
+┃ Red     : #FF0000 ┃ Crimson        : #DC143C ┃
+┃ Green   : #00FF00 ┃ Gainsboro      : #DCDCDC ┃
+┃ Blue    : #0000FF ┃ Misty Rose     : #FFE4E1 ┃
+┃ Yellow  : #FFFF00 ┃ Lavender Blush : #FFF0F5 ┃
+┃ Magenta : #FF00FF ┃ Seashell       : #FFF5EE ┃
+┃ Maroon  : #800000 ┃ Honeydew       : #F0FFF0 ┃
+┃ Purple  : #800080 ┃ Mint Cream     : #F5FFFA ┃
+┃ Indigo  : #4B0082 ┃ Brown          : #A52A2A ┃
+┃ Gold    : #FFD700 ┃ Old Lace       : #FDF5E6 ┃
+┗━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━━━━━━━━┻━━━━━━━━━┛
+ """
