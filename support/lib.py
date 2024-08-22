7ab0e32c745420ed0ca1a3e6a9a91d12e69f2d07
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-08-22 21:37:50 +0800

    Update lib.py

--- support/lib.py
+++ support/lib.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 #Theme-Me/support/lib.py
 #Credit By Zidan IDz
-import os, sys time, requests
+import os, sys, time, requests, getpass
+from logo import theme-me
 
 
 gelap = ' \033[38;5;240m'
@@ -22,13 +24,13 @@ netral = '\033[0m'
 ver = "v1.0.1"
 url = "https://api.github.com/repos/Zidan-ID17/Theme-Me/releases" 
 
-def network(now):
+def version(now):
     try:
       response = requests.get(url)
       response.raise_for_status()
       dat_up = response.json()
-      new_ver = dat_up[0}["name"]
-      if new_ver > now_ver:
+      new_ver = dat_up[0]["name"]
+      if new_ver > now:
         print(f"{putih}[{oren}∆{putih}] new version is available, please update now {oren}!!")
         print(f"{putih}[{oren}∆{putih}] current version {ver} , latest version {new_ver} ")
         sys.exit()
@@ -43,5 +45,27 @@ def runntxt(x):
     sys.stdout.write(i)
     sys.stdout.flush()
     time.sleep(0.1/100)
-    
 
+def entry():
+    time.sleep(1)
+    os.system("clear")
+    runntxt(theme-me)
+    print(f"\n{pxh} For Tokens, you can check YouTube Zeyshyy {netral}")
+    code = getpass.getpass(f"{putih}Enter Token {hijau}>{putih} ").lower()
+    if code == "every0second0hurts":
+        time.sleep(1)
+        print(f"\n{putih} Login Successfully [{hijau}✓{putih}]")
+        time.sleep(1)
+        main()
+    else:
+        time.sleep(1)
+        print(f"\n{putih} Login Failed [{merah}X{putih}]")
+        time.sleep(1)
+        ask = input(f" \n{white} Want to Try Again ? {kuning}(Y/N) > ").lower()
+        if ask == "y":
+            entry()
+        else:
+            time.sleep(1)
+            print(f"\n{putih} Bye Bye 👋\n")
+            time.sleep(1)
+            sys.exit()
