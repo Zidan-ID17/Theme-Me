15e6a22d219d19f6e98bd789bcf541dc7debf213
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-08-22 20:27:32 +0800

    Update logo.py

--- support/logo.py
+++ support/logo.py
@@ -18,7 +18,19 @@ theme-me = (f"""
 {kuning} • {putih}Github  : https://github.com/Zidan-ID17 
 {abu}———————————————————————————————————————————""")
 
-lst_clr = """{abu}
+m_pg = (f"""{putih}              <[{pxm} THEME-ME {netral}{putih}]>
+╔—————————————————————————————————————————┓
+║{pxh}                 THEME                   {netral}{putih}┃
+║{pxh}                  FONT                   {netral}{putih}┃
+║{pxh}               BACKGROUND                {netral}{putih}┃
+╠—————————————————————————————————————————┫
+║{pxh}                RESTORE                  {netral}{putih}┃
+║{pxh}                 ABOUT                   {netral}{putih}┃
+║{pxh}                  EXIT                   {netral}{putih}┃
+╚—————————————————————————————————————————┛
+""")
+
+lst_clr = (f"""{abu}
      Some List of HTML & CSS Color Codes
 ┏━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━━━━━┳━━━━━━━━━┓
 ┃ White   : #FFFFFF ┃ Light Coral    : #F08080 ┃
@@ -32,4 +44,43 @@ lst_clr = """{abu}
 ┃ Indigo  : #4B0082 ┃ Brown          : #A52A2A ┃
 ┃ Gold    : #FFD700 ┃ Old Lace       : #FDF5E6 ┃
 ┗━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━━━━━━━━┻━━━━━━━━━┛
- """
+ """)
+
+th1_pg = (f"""
+       {putih}          [{pxm} THEME {netral}{putih}] {merah}
+╔════════════—•[{pxh} PAGE  1/2 {netral}{merah}]•—════════════•
+╠═══•[{putih}01{merah}]{putih} Infernal Reaper   {merah}
+╠═══════════════════════•[{putih}02{merah}] {putih}Team Liquid {merah}
+╠═══•[{putih}03{merah}]{putih} Todak   {merah}
+╠═══════════════════════•[{putih}04{merah}] {putih}Wings Gaming  {merah}
+╠═══•[{putih}05{merah}] {putih}Blacklist{merah}
+╠═══════════════════════•[{putih}06{merah}] {putih}Onic  {merah}
+╠═══•[{putih}07{merah}] {putih}Fnatic {merah}
+╠═══════════════════════•[{putih}08{merah}] {putih}Faze Clan {merah}
+╠═══•[{putih}09{merah}] {putih}MegaStars {merah}
+╠═══════════════════════•[{putih}10{merah}] {putih}19esports{merah}
+║———————————————————————————————————————————
+╠═══•[{putih}77{merah}] {putih}Next Page {merah}
+║———————————————————————————————————————————
+╚═══•[{putih}99{merah}] {putih}Return to {pxm} Main Page {netral} {putih}
+""")
+
+th2_pg = = (f"""
+       {putih}          [{pxm} THEME {netral}{putih}] {merah}
+╔════════════—•[{pxh} PAGE  2/2 {netral}{merah}]•—════════════•
+╠═══•[{putih}11{merah}] {putih}Ethereal Cranium{merah}
+╠═══════════════════════•[{putih}12{merah}] {putih}Bigetron {merah}
+╠═══•[{putih}13{merah}]{putih} Evil Geniuses   {merah}
+╠═══════════════════════•[{putih}14{merah}] {putih}Team Spirit {merah}
+╠═══•[{putih}15{merah}] {putih}Cloud 9{merah}
+╠═══════════════════════•[{putih}16{merah}] {putih}KeepBest Gaming  {merah}
+╠═══•[{putih}17{merah}] {putih}Invictus Gaming {merah}
+╠═══════════════════════•[{putih}18{merah}] {putih}Iluminate {merah}
+╠═══•[{putih}19{merah}] {putih}496 Gaming {merah}
+╠═══════════════════════•[{putih}20{merah}] {putih}Team SoloMid{merah}
+║———————————————————————————————————————————
+╠═══•[{putih}77{merah}] {putih}Next Page {merah}
+╠═══•[{putih}88{merah}] {putih}Previous Page{merah}
+║———————————————————————————————————————————
+╚═══•[{putih}99{merah}] {putih}Return to {pxm} Main Page {netral} {putih}
+""")
