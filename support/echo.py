0351faf79e3b3de2dabb72eba7ecb289a47c3c15
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-09-30 16:21:52 +0800

    Update and rename logo.py to echo.py

---
+++ support/echo.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+# Theme-Me/support/logo.py
+# Credit By Zidan IDz
+
+
+gelap = "\033[38;5;240m"
+merah =  "\033[91m"
+hijau = "\033[92m"
+kuning = "\033[93m"
+biru = "\033[94m"
+ungu = "\033[95m"
+abu = "\033[90m"
+putih = "\033[97m"
+oren = "\033[38;5;208m"
+pxm = "\033[97;41m"
+mxp = "\033[91;47m"
+pxm = "\033[97;41m"
+axh = "\033[30;100m"
+pxh = "\033[30;47m"
+netral = "\033[0m"
+
+theme_me = (f"""
+{merah}████████╗██╗  ██╗███████╗{putih}███╗   ███╗███████╗
+{merah}╚══██╔══╝██║  ██║██╔════╝{putih}████╗ ████║██╔════╝
+{merah}   ██║   ███████║█████╗  {putih}██╔████╔██║█████╗  
+{merah}   ██║   ██╔══██║██╔══╝  {putih}██║╚██╔╝██║██╔══╝  
+{merah}   ██║   ██║  ██║███████╗{putih}██║ ╚═╝ ██║███████╗
+{merah}   ╚═╝   ╚═╝  ╚═╝╚══════╝{putih}╚═╝     ╚═╝╚══════╝
+{abu}               .: B E T A :.
+
+{kuning} —• {putih}Author  : {pxm} ZidanIDz {netral}
+{kuning} • {putih}YouTube : ZeyShyy.
+{kuning} • {putih}Github  : https://github.com/Zidan-ID17 
+{abu}——————————————————————————————————————————""")
+
+m_pg = (f"""{putih}              <[{pxm} THEME-ME {netral}{putih}]>
+╔—————————————————————————————————————————┓
+║{pxh}                 THEME                   {netral}{putih}┃
+║{pxh}                  FONT                   {netral}{putih}┃
+║{pxh}               BACKGROUND                {netral}{putih}┃
+╠—————————————————————————————————————————┫
+║{pxh}                RESTORE                  {netral}{putih}┃
+║{pxh}                 ABOUT                   {netral}{putih}┃
+║{pxh}                  EXIT                   {netral}{putih}┃
+╚—————————————————————————————————————————┛
+""")
+
+lst_clr = (f"""{abu}
+     some list of HTML & CSS color codes
+┏━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━━━━━┳━━━━━━━━━┓
+┃ white   : #FFFFFF ┃ light coral    : #F08080 ┃
+┃ red     : #FF0000 ┃ crimson        : #DC143C ┃
+┃ green   : #00FF00 ┃ gainsboro      : #DCDCDC ┃
+┃ blue    : #0000FF ┃ misty rose     : #FFE4E1 ┃
+┃ yellow  : #FFFF00 ┃ lavender blush : #FFF0F5 ┃
+┃ magenta : #FF00FF ┃ seashell       : #FFF5EE ┃
+┃ maroon  : #800000 ┃ honeydew       : #F0FFF0 ┃
+┃ purple  : #800080 ┃ mint cream     : #F5FFFA ┃
+┃ indigo  : #4B0082 ┃ brown          : #A52A2A ┃
+┃ gold    : #FFD700 ┃ old lace       : #FDF5E6 ┃
+┗━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━━━━━━━━┻━━━━━━━━━┛
+ """)
+
+th1_pg = (f"""
+       {putih}          [{pxm} THEME {netral}{putih}] {merah}
+╔════════════—•[{pxh} PAGE  1/2 {netral}{merah}]•—════════════•
+╠═══•[{putih}01{merah}]{putih} infernal-reaper   {merah}
+╠═══════════════════════•[{putih}02{merah}] {putih}team-liquid {merah}
+╠═══•[{putih}03{merah}]{putih} todak   {merah}
+╠═══════════════════════•[{putih}04{merah}] {putih}wings-gaming  {merah}
+╠═══•[{putih}05{merah}] {putih}blacklist{merah}
+╠═══════════════════════•[{putih}06{merah}] {putih}onic  {merah}
+╠═══•[{putih}07{merah}] {putih}fnatic {merah}
+╠═══════════════════════•[{putih}08{merah}] {putih}faze-clan {merah}
+╠═══•[{putih}09{merah}] {putih}megastars {merah}
+╠═══════════════════════•[{putih}10{merah}] {putih}19esports{merah}
+║———————————————————————————————————————————
+╠═══•[{putih}77{merah}] {putih}next page {merah}
+║———————————————————————————————————————————
+╚═══•[{putih}99{merah}] {putih}return to {pxm} main page {netral} {putih}
+""")
+
+th2_pg = (f"""
+       {putih}          [{pxm} THEME {netral}{putih}] {merah}
+╔════════════—•[{pxh} PAGE  2/2 {netral}{merah}]•—════════════•
+╠═══•[{putih}11{merah}] {putih}ethereal-cranium{merah}
+╠═══════════════════════•[{putih}12{merah}] {putih}bigetron {merah}
+╠═══•[{putih}13{merah}]{putih} evil-geniuses   {merah}
+╠═══════════════════════•[{putih}14{merah}] {putih}team-spirit {merah}
+╠═══•[{putih}15{merah}] {putih}cloud-9{merah}
+╠═══════════════════════•[{putih}16{merah}] {putih}keepbest-gaming  {merah}
+╠═══•[{putih}17{merah}] {putih}invictus-gaming {merah}
+╠═══════════════════════•[{putih}18{merah}] {putih}iluminate {merah}
+╠═══•[{putih}19{merah}] {putih}496-Gaming {merah}
+╠═══════════════════════•[{putih}20{merah}] {putih}team-solomid{merah}
+║———————————————————————————————————————————
+╠═══•[{putih}88{merah}] {putih}previous page{merah}
+║———————————————————————————————————————————
+╚═══•[{putih}99{merah}] {putih}return to {pxm} main page {netral} {putih}
+""")
+
+bkg1_pg = (f"""
+       {putih}       [{pxm} BACKGROUND {netral}{putih}] {merah}
+╔════════════—•[{pxh} PAGE  1/2 {netral}{merah}]•—════════════•
+╠═══•[{putih}01{merah}]{putih} 3024-dark  {merah}
+╠═══════════════════════•[{putih}02{merah}] {putih}apathy-dark{merah}
+╠═══•[{putih}03{merah}]{putih} ashes-dark  {merah}
+╠═══════════════════════•[{putih}04{merah}] {putih}atelierdune-dark{merah}
+╠═══•[{putih}05{merah}] {putih}atelierforest-dark{merah}
+╠═══════════════════════•[{putih}06{merah}] {putih}atelierhealt-dark {merah}
+╠═══•[{putih}07{merah}] {putih}atelierlakeside-dark{merah}
+╠═══════════════════════•[{putih}08{merah}] {putih}atelierseaside-dark{merah}
+╠═══•[{putih}09{merah}] {putih}bespin-dark{merah}
+╠═══════════════════════•[{putih}10{merah}] {putih}brewer-dark{merah}
+║———————————————————————————————————————————
+╠═══•[{putih}77{merah}] {putih}next page {merah}
+║———————————————————————————————————————————
+╚═══•[{putih}99{merah}] {putih}return to {pxm} main page {netral} {putih}
+""")
+
+bkg2_pg = (f"""
+       {putih}       [{pxm} BACKGROUND {netral}{putih}] {merah}
+╔════════════—•[{pxh} PAGE  2/2 {netral}{merah}]•—════════════•
+╠═══•[{putih}11{merah}]{putih} bright-dark  {merah}
+╠═══════════════════════•[{putih}12{merah}] {putih}chalk-dark{merah}
+╠═══•[{putih}13{merah}]{putih} codeschool-dark  {merah}
+╠═══════════════════════•[{putih}14{merah}] {putih}colors-dark{merah}
+╠═══•[{putih}15{merah}] {putih}default-dark{merah}
+╠═══════════════════════•[{putih}16{merah}] {putih}eighties-dark {merah}
+╠═══•[{putih}17{merah}] {putih}embers-dark{merah}
+╠═══════════════════════•[{putih}18{merah}] {putih}flat-dark{merah}
+╠═══•[{putih}19{merah}] {putih}google-dark{merah}
+╠═══════════════════════•[{putih}20{merah}] {putih}grayscale-dark{merah}
+║———————————————————————————————————————————
+╠═══•[{putih}77{merah}] {putih}next page {merah}
+╠═══•[{putih}88{merah}] {putih}previous page {merah}
+║———————————————————————————————————————————
+╚═══•[{putih}99{merah}] {putih}return to {pxm} main page {netral} {putih}
+""")
+
+fnt1_pg = (f"""{putih}
+                 [{pxm} FONT {netral}{putih}] {merah}
+╔════════════—•[{pxh} PAGE  1/2 {netral}{merah}]•—════════════•
+╠═══•[{putih}01{merah}]{putih} anonymous-pro  {merah}
+╠═══════════════════════•[{merah}02{merah}] {putih}courier-prime{merah}
+╠═══•[{putih}03{merah}]{putih} dejavu {merah}
+╠═══════════════════════•[{putih}04{merah}] {putih}fantasque{merah}
+╠═══•[{putih}05{merah}] {putih}fira{merah}
+╠═══════════════════════•[{putih}06{merah}] {putih}firacode {merah}
+╠═══•[{putih}07{merah}] {putih}gnu-freefont{merah}
+╠═══════════════════════•[{putih}08{merah}] {putih}go{merah}
+╠═══•[{putih}09{merah}] {putih}hack {merah}
+╠═══════════════════════•[{putih}10{merah}] {putih}hermit{merah}
+║———————————————————————————————————————————
+╠═══•[{putih}77{merah}] {putih}next page {merah}
+║———————————————————————————————————————————
+╚═══•[{putih}99{merah}] {putih}return to {pxm} main page {netral} {putih}
+""")
+
+fnt2_pg = (f"""{putih}
+                 [{pxm} FONT {netral}{putih}] {merah}
+╔════════════—•[{pxh} PAGE  2/2 {netral}{merah}]•—════════════•
+╠═══•[{putih}11{merah}]{putih} inconsolata  {merah}
+╠═══════════════════════•[{putih}12{merah}] {putih}iosevka{merah}
+╠═══•[{putih}13{merah}]{putih} liberation-mono {merah}
+╠═══════════════════════•[{putih}14{merah}] {putih}meslo{merah}
+╠═══•[{putih}15{merah}] {putih}monofur{merah}
+╠═══════════════════════•[{putih}16{merah}] {putih}monoid {merah}
+╠═══•[{putih}17{merah}] {putih}open-dyslexic{merah}
+╠═══════════════════════•[{putih}18{merah}] {putih}roboto{merah}
+╠═══•[{putih}19{merah}] {putih}source-code-pro {merah}
+╠═══════════════════════•[{putih}20{merah}] {putih}ubuntu{merah}
+║———————————————————————————————————————————
+╠═══•[{putih}88{merah}] {putih}previous page {merah}
+║———————————————————————————————————————————
+╚═══•[{putih}99{merah}] {putih}return to {pxm} main page {netral} {putih}
+""")
+
+note = (f"""{abu}
+╔═══════════════════════════════════════════════╗
+║                   {kuning}ABOUT{abu}                       ║
+║ theme-Me is a simple tool built in python.    ║
+║ this tools is used to change the appearance   ║
+║ of Termux.                                    ║ 
+║ this tools has several customization options, ║
+║ among others :                                ║
+║     ★ theme                                   ║
+║     ★ font                                    ║
+║     ★ background                              ║
+║ some options may not be complete because this ║
+║ tool's still a BETA current.                  ║
+║                                               ║
+║ >> current version is {putih}BETA {abu}({kuning}0.3.5{abu})            ║
+╚═══════════════════════════════════════════════╝
+credit by Zidan IDz
+""")
