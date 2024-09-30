dfe37370b55fbb9b230fd891b84c420668651a32
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-09-30 13:02:10 +0800

    Update update.py

--- update.py
+++ update.py
@@ -9,6 +9,11 @@ def main():
         print(f"Menghapus folder lama: {theme_me_dir}")
         os.system(f"rm -rf {theme_me_dir}")
 
+    parent_dir = os.path.dirname(theme_me_dir)
+    if not os.path.exists(parent_dir):
+        print(f"Membuat direktori induk: {parent_dir}")
+        os.makedirs(parent_dir)
+
     print("Meng-clone repository...")
     result = subprocess.run(['git', 'clone', 'https://github.com/Zidan-ID17/Theme-Me'], 
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
