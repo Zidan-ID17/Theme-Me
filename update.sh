8c64c49b651910eb7c6c836297a86e835855615b
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-09-30 12:48:25 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 
 if [ -d "../Theme-Me" ]; then
     rm -rf ../Theme-Me
 fi
 
-
 git clone https://github.com/Zidan-ID17/Theme-Me ../Theme-Me
-
 
 cd ../Theme-Me || { echo "Failed to enter Theme-Me directory"; exit 1; }
