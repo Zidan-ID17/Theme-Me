4d486d0f62b66cc7f0812382a73e302766e8a2f1
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-10-02 18:10:24 +0800

    Add files via upload

---
+++ main/th18.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# Theme-Me/main/th18.py
+# Credit by Zidan IDz
+import os, sys, time, shutil
+from support.echo import theme_me, abu, pxh, netral, putih, kuning, hijau, merah
+
+
+def th18():
+    os.system("clear")
+    print(theme_me)
+    print(f"          {abu}THEME-ME {pxh} Iluminate {netral}")
+    print(f"{putih}({kuning}*{putih}) {abu}'username' and 'team name' will be displayed below the logo")
+    username = input(f"\n{putih}enter username   {hijau}>{putih} ")
+    while not username.strip() or '"' in username or "'" in username:
+        print(f"{abu}({merah}X{abu}) must not be blank or contain quotation marks {merah}!!")
+        username = input(f"\n{putih}enter username   {hijau}>{putih} ")
+        continue
+    time.sleep(0.5)
+    team = input(f"\n{putih}enter team name  {hijau}>{putih}")
+    while not team.strip() or '"' in team or "'" in team:
+        print(f"{abu}({merah}X{abu}) must not be blank or contain quotation marks {merah}!!")
+        team = input(f"\n{putih}enter team name  {hijau}>{putih}")
+        continue
+    file = "theme.py"
+    fill = """#Created with the THEME-ME coded by Zidan IDz 
+import os, sys, time
+
+def run(a):
+    for n in a + '\\n':
+        sys.stdout.write(n)
+        sys.stdout.flush()
+        time.sleep(0.1/100)
+
+logo = '''
+\033[38;5;214m             .~?Y7               7Y7^.          
+          .!J5G5^       :!.       ~PG5J~.       
+         ~PGGGJ.        .!.        .YGGG5:      
+         YGPG?    .:~7?JJJJJ?7~:.    JGPGJ      
+     :? :PPG?  :!JPGP5YJJ?JJY5PGPJ!:  YGPP.:?.  
+    :5Y ~GGY ^JPGP?~:.       .:~?5GPJ:.5GP: 5Y. 
+   .YGJ !GP~?PGGJ:   Y55YYY55Y   .~YGP7!PG^ YGJ 
+   7GGY ~GYYGPGY     ^~~5G5~~^      7PGY5G: 5GG!
+   5PPP.:PPGPGG!    .7?^JGJ^?77~:.   !GGPP.:PPG5
+   5GPG! YGG5YGY.   .PG!JGJ!GG55PPJ~  YGG? 7GPG5
+   :YGGP.~GG?.JGP?~::PG!JGJ!GP: :!5GY:?GG^:PGGJ.
+   ..!PG5^5G5  :?5PPPGG!JGJ!GP:    YG55GP^5G5~..
+   :Y~.7P55GG7    .^~~!:YGJ:!!.    7GGPG55P!:!Y.
+    ~GY~:!YPGG?.     !!!5G5!!!    .5GPGPJ!:!5G^ 
+     ~PG5?~^75G5!.   ?JJJJJJJ?   ^YGP57^~?PGP^  
+      :YGGG5J?5GGPJ!^.       .^!YGGP5JJPGGGJ.   
+        ~?YY5555555PGP5YYYYY5PGP5555555YJ?~     
+           :!????JY5J!5GGGGGY!Y5YJ????!.        
+            .^7YP57^^JGPPYPPP?:^?5PY7^.         
+                .  :Y55P? JP55J.  .    '''
+run(logo)
+print('''              \033[97m Username : \033[30;48;5;208m """+username+""" \033[0m''')
+print('''            \033[97m Team :\033[90m """+team+""" \033[0m''')  """
+    with open(file, "w") as y:
+        y.write(fill)
+    of = "theme.py"
+    into = os.path.join(os.environ["HOME"], "..", "usr", "etc", "theme.py")
+    und = os.path.join(os.environ["HOME"], "..", "usr", "etc", "motd")
+    if os.path.exists(into):
+        os.remove(into)
+    elif os.path.exists(und):
+        os.remove(und)
+
+    while True:
+        try:
+            shutil.move(of, into)
+            break
+        except Exception as e:
+            with open(file, "w") as y:
+                y.write(fill)
+            time.sleep(1)
---
+++ main/th20.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# Theme-Me/main/th20.py
+# Credit by Zidan IDz
+import os, sys, time, shutil
+from support.echo import theme_me, abu, pxh, netral, putih, kuning, hijau, merah
+
+
+def th20():
+    os.system("clear")
+    print(theme_me)
+    print(f"         {abu}THEME-ME {pxh} Team SoloMid {netral}")
+    print(f"{putih}({kuning}*{putih}) {abu}'username' and 'team name' will be displayed below the logo")
+    username = input(f"\n{putih}enter username   {hijau}>{putih} ")
+    while not username.strip() or '"' in username or "'" in username:
+        print(f"{abu}({merah}X{abu}) must not be blank or contain quotation marks {merah}!!")
+        username = input(f"\n{putih}enter username   {hijau}>{putih} ")
+        continue
+    time.sleep(0.5)
+    team = input(f"\n{putih}enter team name  {hijau}>{putih}")
+    while not team.strip() or '"' in team or "'" in team:
+        print(f"{abu}({merah}X{abu}) must not be blank or contain quotation marks {merah}!!")
+        team = input(f"\n{putih}enter team name  {hijau}>{putih}")
+        continue
+    file = "theme.py"
+    fill = """#Created with the THEME-ME coded by Zidan IDz 
+import os, sys, time
+
+def run(a):
+    for n in a + '\\n':
+        sys.stdout.write(n)
+        sys.stdout.flush()
+        time.sleep(0.1/100)
+
+logo = '''
+\033[97m                                                  
+                .^!?YPGGGGGGPY?!^.                
+            .~YG&@@@@&#@@@@#&@@@@&GY!.            
+          ~5&@@@GY!^:. G@@G .:^!YG&@@&5~          
+        !B@@&P!.:~JPB: G@@G :G57^..!P&@@B!        
+      ^G@@&J..7P&@@@@^ G@@G :@@@@&P! .J&@@G^      
+     7&@@5. J#@@#B@@@^ G@@G :@@&P&@@#? .5@@&7     
+    ?@@@7 ~#@@#?.7@@@^ G@@G :@@# ^@@@@B^ 7@@@?    
+   !@@@7 7@@@5.  7@@&: G@@G :@@# :&@@@@&! 7@@@!   
+   B@@5 ^@@@Y     :::  G@@G :@@# :&@#J@@&^ 5@@B.  
+  ~@@@^ P@@&!~~~~~~~~. G@@G :@@# :&@# 5@@P ^@@@!  
+  ?@@#..&@@@@@@@@@@@@^ G@@G :@@# :&@# :@@&. #@@J  
+  ?@@#. 7???????75@@@^ G@@G :@@# :&@# :&@&. #@@J  
+  ~@@@^          !@@@^ G@@G :@@# :&@# :&@G ^@@@!  
+   B@@5 :B##Y    7@@@^ G@@G :@@# :&@# :&@~ 5@@B   
+   ~@@@7 7@@@5.  7@@@^ G@@G :@@# :&@# :&? 7@@@!   
+    ?@@@? ~#@@#7 !@@@^ G@@G :@@# :&@# .! 7@@@?    
+     7&@@5. J&@@#G@@@^ G@@G :@@# :@@B  .5@@&7     
+      ^G@@&J..7G&@@@@^ G@@G :@@# :G?::J&@@G^      
+        !B@@@P!::~JPB: G@@G :BGJ  :!5&@@B!        
+          ~5&@@@GY!^^  G@@G  ^~!YG&@@&5~          
+            .~YG&@@@@^ G@@G :&@@@&GY!.            
+                .^!?Y: YBGY .Y?!^.                '''
+run(logo)
+print('''              \033[97m Username : \033[30;47m """+username+""" \033[0m''')
+print('''            \033[97m Team :\033[90m """+team+""" \033[0m''')  """
+    with open(file, "w") as y:
+        y.write(fill)
+    of = "theme.py"
+    into = os.path.join(os.environ["HOME"], "..", "usr", "etc", "theme.py")
+    und = os.path.join(os.environ["HOME"], "..", "usr", "etc", "motd")
+    if os.path.exists(into):
+        os.remove(into)
+    elif os.path.exists(und):
+        os.remove(und)
+
+    while True:
+        try:
+            shutil.move(of, into)
+            break
+        except Exception as e:
+            with open(file, "w") as y:
+                y.write(fill)
+            time.sleep(1)
