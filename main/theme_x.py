f59fdde332305d948f46b922644ce6340e13332b
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-10-02 17:06:48 +0800

    Create theme_x.py

---
+++ main/theme_x.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Theme-Me/main/theme_x.py
+# Credit by Zidan IDz
+from .th11 import th11
+from .th12 import th12
+from .th13 import th13
+from .th14 import th14
+from .th15 import th15
+from .th16 import th16
+from .th17 import th17
+from .th18 import th18
+from .th19 import th19
+from .th20 import th20
+
+def theme_x(i):
+    if i == "A":
+        th11()
+    elif i == "B":
+        th12()
+    elif i == "C":
+        th13()
+    elif i == "D":
+        th14()
+    elif i == "E":
+        th15()
+    elif i == "F":
+        th16()
+    elif i == "G":
+        th17()
+    elif i == "H":
+        th18()
+    elif i == "I":
+        th19()
+    elif i == "J":
+        th20()
+    else:
+        return
