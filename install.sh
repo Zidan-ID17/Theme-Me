088a4b8fd35286064c7f9569370812e224970a60
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-10-16 13:39:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,17 @@
 
 echo -e "\033[92m please wait..."
 sleep 1
-pkg install nano
-pkg install python
-pip install requests
+pkg install nano -y
+pkg install python -y
+
+while ! python -c "import requests" &> /dev/null; do
+    echo -e "\033[91m 'requests' module not found. Installing..."
+    pip install requests
+    sleep 1
+done
+
+echo -e "\033[92m Requests module is installed!"
+
 echo -e "\033[92m entering the tools..."
 sleep 2
 python umbrella.py
