6acd710727e063a7254c63b57413b6eaa1f8e846
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-10-16 23:19:04 +0800

    Update __init__.py

--- support/__init__.py
+++ support/__init__.py
@@ -1,1 +1,4 @@
 # Theme-Me/support/__init__.py
+# Theme-Me/support/__init__.py
+from .lib import version, message, entry, runntxt, load
+from .echo import putih, hijau, pxh, merah, abu, netral, theme_me, m_pg, th1_pg, th2_pg, bkg1_pg, bkg2_pg, fnt1_pg, fnt2_pg
