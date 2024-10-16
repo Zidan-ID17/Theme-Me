285f5c89dc38c1a27c41013d0dc6a19dbe05d59e
Zidan IDz <60657412+Zidan-ID17@users.noreply.github.com>
2024-10-16 23:20:54 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,1 +1,8 @@
 # Theme-Me/main/__init__.py
+from .bashrc import bashrc
+from .theme_y import theme_y
+from .theme_x import theme_x
+from .font_y import font_y
+from .font_x import font_x
+from .background_x import background_x
+from .background_y import background_y 
